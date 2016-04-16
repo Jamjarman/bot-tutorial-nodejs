42f01d94b8ddfdd4c18dde1e7142bf9ca6b482c9
Jamie Gilkeson <rangedenemy@gmail.com>
2016-04-15 21:31:06 -0400

    Updated to run on server and properly respond to commands. Git commands still require password which needs to be fixed and pm2 command hasn't been tested yet. Cat and ls work locally, should work remotely as there is no actual difference

--- bot.js
+++ bot.js
@@ -2,28 +2,113 @@ var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
+var name = process.env.name;
+var help="Usage: \n/"+name+" introduces all commands\nbuild directory will use git to pull and then restart pm2 for the specified directory\nls directory will return the contents of the given directories\ncat file will return the contents of the spcified files\nhelp will display this mneu";
 
 function respond() {
-  var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
-
-  if(request.text && botRegex.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage();
-    this.res.end();
-  } else {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
-  }
+    console.log(name);
+    var request = JSON.parse(this.req.chunks[0]);
+    //console.log(request);
+    //console.log(request.text);
+    //console.log(request.sender_id);
+    var botRgx=new RegExp("^^/"+name);
+    //if message matches bots command string parse it
+    if(request.text && botRgx.test(request.text)){
+	console.log("Checking query");
+	var input=request.text;
+	console.log(input);
+	var comB=input.indexOf(" ");
+	var comBE=input.indexOf(" ", comB+1);
+	if(comBE<0)
+	    comBE=input.length;
+	console.log("comB "+comB+", comBE "+comBE);
+	//get the command as the first string after the directive
+	var com=input.substring(comB+1, comBE);
+	console.log(com);
+	input=input.substring(comBE+1);
+	console.log(input);
+	var argsL=[];
+	//iterate over the rest of the string to get any arguments
+	while(input.indexOf(" ")>=0){
+	    var argT=input.substring(0, input.indexOf(" ")+1);
+	    console.log(argT);
+	    argsL.push(argT);
+	    input=input.substring(input.indexOf(" ")+1);
+	    console.log(input);
+	}
+	if(input.length>0){
+	    argsL.push(input);
+	}
+	console.log(argsL);
+	try{
+	    if(com=="help"){
+		postMessage(help);
+	    }
+	    else if(com=="ls"){
+		var lsOut=new run_cmd([com], [argsL]);
+	    }
+	    else if(com=="cat"){
+		var catOut=new run_cmd([com], [argsL]);
+	    }
+	    else if(com=="build"){
+		var exec = require('child_process').exec;
+		exec('git status', {cwd: argsL[0]}, (error, stdout, stderr) => {
+		    if(error){
+			throw error;
+		    }
+		    postMessage(stdout);
+		});
+		console.log("Hello");
+		exec('grep "main" package.json', {cwd: argsL[0]}, (error, stdout, stderr) => {
+		    console.log("Getting main file");
+		    if(error)
+			throw error;
+		    var spaceLoc=stdout.indexOf(" ", 5);
+		    var endLoc=stdout.indexOf("\"", spaceLoc+2);
+		    console.log(stdout+" "+spaceLoc+" "+endLoc);
+		    var mainfile=stdout.substring(spaceLoc+2, endLoc);
+		    console.log(mainfile);
+		    exec('pm2 restart '+mainfile, {cwd: argsL[0]}, (error, stdout, stderr) => {
+			if(error)
+			    throw error;
+			postMessage(stdout);
+		    });
+		});
+		
+		
+	    }
+	    else{
+		console.log("Command not found");
+		postMessage(help);
+	    }
+	}
+	catch(err){
+	    console.log(err);
+	    postMessage(help);
+	}
+    }
+    //postMessage();
 }
 
-function postMessage() {
-  var botResponse, options, body, botReq;
+function run_cmd(comd, args){
+    for(var i=0; i<comd.length; i++){
+	var spawn=require('child_process').spawn, child=spawn(comd[i], args[i]), me=this;
+	
+	child.stdout.on('data', function(buffer){
+	    me.stdout += buffer.toString();
+	});
+	child.stdout.on('end', function(){
+	    postMessage(me.stdout.substring(9));
+	});
+    }
+    
+}
+    
 
-  botResponse = cool();
+function postMessage(botResponse) {
+  var options, body, botReq;
 
-  options = {
+   options = {
     hostname: 'api.groupme.com',
     path: '/v3/bots/post',
     method: 'POST'
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var http, director, cool, bot, router, server, port;
+var dotenv=require('dotenv');
+dotenv.load();
 
 http        = require('http');
 director    = require('director');
@@ -6,7 +8,7 @@ cool        = require('cool-ascii-faces');
 bot         = require('./bot.js');
 
 router = new director.http.Router({
-  '/' : {
+  '/callback/' : {
     post: bot.respond,
     get: ping
   }
@@ -24,7 +26,7 @@ server = http.createServer(function (req, res) {
   });
 });
 
-port = Number(process.env.PORT || 5000);
+port = Number(process.env.PORT || 8075);
 server.listen(port);
 
 function ping() {
